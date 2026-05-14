e263e1d4b646f6596ea9101b8eb58e3452c12d80
Renato <renato.flair@hotmail.com>
2026-05-14 17:27:28 -0300

    fix: use correct supabase jwt anon key

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,7 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl = "https://pbosucrtmmngandfhttm.supabase.co";
-const supabaseAnonKey = "sb_publishable_2AyCIufC1CIvThpHV3azbA_XiNggiou";
+const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBib3N1Y3JjdG5ubmdqeHJkaHRtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg3Njk0OTMsImV4cCI6MjA5NDM0NTQ5M30.29X6DTs7JbUI5jxAVTCSNuQoFRmwgdZjfmI0GQ76kDY";
 
 if (!supabaseUrl || !supabaseAnonKey) {
   console.warn("Supabase URL or Anon Key is missing. Check your .env.local file.");
